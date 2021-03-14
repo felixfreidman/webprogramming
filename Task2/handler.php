b3068c6aaebd6c31f74f9ea526b5ecfb651dd7e2
Felix Freidman <felixfreidman@gmail.com>
2021-03-15 03:50:05 +0800

    3:49AM Commit Сделал второе задание опять как ублюдок все одним коммитом

--- Task2/handler.php
+++ Task2/handler.php
@@ -8,29 +8,29 @@
     <link rel="stylesheet" href="./index.css" />
   </head>
   <body>
-
     <div class = "main">
-      <h1><a href = "./Task2/index.html">Перейти на функциональный способ</a></h1>
       <table class = "main__table">
 		<?php
 //Table Header Creator
 $rowCounter = 1;
 $columnCounter = 1;
 $miltiplier = 1;
+$rowAmount = $_GET['rows'];
+$columnAmount = $_GET['columns'];
 echo '<tr>';
 echo '<td class = "table__cell_table-start"></td>';
 
-while ($rowCounter < 11) {
+while ($rowCounter < $columnAmount + 1) {
     echo '<td class = "table__cell_row-header">' . $rowCounter . '</td>';
     $rowCounter++;
 }
 echo '</tr>';
 //Table Content Creator
 
-for ($rowCounter = 1; $rowCounter < 11; $rowCounter++) {
+for ($rowCounter = 1; $rowCounter < $rowAmount + 1; $rowCounter++) {
     echo '<tr>';
     echo '<td class = "table__cell_row-counter">' . $rowCounter . '</td>';
-    for ($columnCounter = 1; $columnCounter < 11; $columnCounter++) {
+    for ($columnCounter = 1; $columnCounter < $columnAmount + 1; $columnCounter++) {
         if ($columnCounter === $miltiplier) {
             echo '<td class = "table__cell-diagonale">' . $miltiplier * $columnCounter . '</td>';
         } else {
