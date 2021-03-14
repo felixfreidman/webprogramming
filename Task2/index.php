1feefb49b4e8f78ce86fea118f58c1015ffe856b
Felix Freidman <felixfreidman@gmail.com>
2021-03-15 03:02:25 +0800

    3:02 AM Commit Создал базовый проект таблицы умножения

---
+++ Task2/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <link rel="stylesheet" href="./index.css" />
+  </head>
+  <body>
+    <div class = "main">
+      <table class = "main__table">
+		<?php 
+    //Table Header Creator
+    $rowCounter = 1;
+    $columnCounter = 1;
+    $miltiplier = 1;
+    echo '<tr>';
+    echo '<td class = "table__cell_table-start"></td>';
+
+    while ($rowCounter < 11) {
+      echo '<td class = "table__cell_row-header">' . $rowCounter . '</td>';
+      $rowCounter++;
+    }
+    echo '</tr>';
+    //Table Content Creator
+    
+    for($rowCounter = 1; $rowCounter < 11; $rowCounter++) {
+      echo '<tr>';
+      echo '<td class = "table__cell_row-counter">'. $rowCounter . '</td>';
+      for($columnCounter = 1; $columnCounter < 11; $columnCounter++) {
+        if($columnCounter === $miltiplier) {
+          echo '<td class = "table__cell-diagonale">' . $miltiplier * $columnCounter . '</td>'; 
+        } else {
+        echo '<td class = "table__cell">' . $miltiplier * $columnCounter . '</td>';
+        }
+      }
+      $miltiplier++;
+      echo '</tr>';
+    }
+    ?>
+	</table>
+    </div>
+  </body>
+</html>
